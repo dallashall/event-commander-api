7518d4545eb595d225d49c004e64b7377f3b01e1
dallashall <dallas.hall@gmail.com>
2017-04-15 15:06:59 -0700

    Append routes

---
+++ EventCommander/app/helpers/api/team_members_sessions_helper.rb
@@ -0,0 +1,2 @@
+module Api::TeamMembersSessionsHelper
+end
--- EventCommander/config/routes.rb
+++ EventCommander/config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users
-    post    "/sign-in"  => "sessions#create"
-    delete  "/sign-out" => "sessions#destroy"
+    post    "/sign_in"  => "sessions#create"
+    delete  "/sign_out" => "sessions#destroy"
     resources :events
     resources :teams
     resources :team_members
     resources :tasks
     resources :details
+    post "/team_members_sessions" => "team_members_sessions#create"
+    delete "/team_members_sessions" => "team_members_session#destroy"
   end
-  resources :users
   post "/sign-in" => "sessions#make"
   post "/sign-out" => "sessions#unmake"
 end
