e0f8c1b38dc942a6dae1c1d7b9a20a5903047bf3
dallashall <dallas.hall@gmail.com>
2017-04-15 21:29:48 -0700

    Adds CRUD to Events

--- EventCommander/app/controllers/api/events_controller.rb
+++ EventCommander/app/controllers/api/events_controller.rb
@@ -1,2 +1,49 @@
+# AR Model for event object
 class Api::EventsController < ApplicationController
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      render :show
+    else
+      render @event.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @event = selected_event
+    if @event
+      @event.destroy
+      render :show
+    else
+      render @event.errors.full_messages, status: 401
+    end
+  end
+
+  def update
+    @event = selected_event
+    if @event && @event.user_id == current_user.id
+      @event.update_attributes(event_params)
+      render :show
+    else
+      render @event.errors.full_messages, status: 401
+    end
+  end
+
+  def show
+    @event = selected_event
+  end
+
+  def index
+    @events = current_user.events
+  end
+
+  private
+
+  def selected_event
+    Event.find_by(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:user_id, :title, :event_date)
+  end
 end
--- EventCommander/app/controllers/api/users_controller.rb
+++ EventCommander/app/controllers/api/users_controller.rb
@@ -1,3 +1,4 @@
+# AR Model for user object
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
@@ -5,6 +6,16 @@ class Api::UsersController < ApplicationController
       render json: @user.username
     else
       render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user
+      user.destroy
+      render json: ["Successfully removed user"]
+    else
+      render json: ["Failed to find user."]
     end
   end
 end
