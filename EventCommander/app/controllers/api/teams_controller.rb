147e5556cd864c4269e0fd45fc8eb71cb7d5ed47
dallashall <dallas.hall@gmail.com>
2017-04-15 21:40:49 -0700

    Adds CRUD to teams.

--- EventCommander/app/controllers/api/teams_controller.rb
+++ EventCommander/app/controllers/api/teams_controller.rb
@@ -1,2 +1,49 @@
+# AR Model for team object
 class Api::TeamsController < ApplicationController
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      render :show
+    else
+      render @team.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @team = selected_team
+    if @team
+      @team.destroy
+      render :show
+    else
+      render ["Could not find team"], status: 401
+    end
+  end
+
+  def update
+    @team = selected_team
+    if @team && @team.user.id == current_user.id
+      @team.update_attributes(team_params)
+      render :show
+    else
+      render @team.errors.full_messages, status: 401
+    end
+  end
+
+  def show
+    @team = selected_team
+  end
+
+  def index
+    @teams = current_user.teams
+  end
+
+  private
+
+  def selected_team
+    Team.find_by(id: params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:event_id, :name)
+  end
 end
--- EventCommander/app/models/team.rb
+++ EventCommander/app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   validates :event, :name, presence: true
   belongs_to :event
+  has_one :user, through: :event
   has_many :detail_assignments
   has_many :details, through: :detail_assignments
 end
