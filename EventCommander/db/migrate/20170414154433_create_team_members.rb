5946c681bea19e7aa535402c19ac4cb5061caaa4
dallashall <dallas.hall@gmail.com>
2017-04-15 21:06:53 -0700

    Adds DB-level validations

--- EventCommander/db/migrate/20170414154433_create_team_members.rb
+++ EventCommander/db/migrate/20170414154433_create_team_members.rb
@@ -1,9 +1,10 @@
 class CreateTeamMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :team_members do |t|
-      t.string :email
-      t.string :name
-      t.integer :team_id
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :single_use_token, null: false
+      t.integer :team_id, null: false
 
       t.timestamps
     end
